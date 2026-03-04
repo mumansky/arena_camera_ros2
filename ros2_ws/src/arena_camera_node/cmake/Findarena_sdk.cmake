b03d05526d08d5294fd16321cdf682bf9e9b72a8
Mark Umansky <mumansky@vt.edu>
2026-03-03 22:36:34 -0500

    first build on orin

--- ros2_ws/src/arena_camera_node/cmake/Findarena_sdk.cmake
+++ ros2_ws/src/arena_camera_node/cmake/Findarena_sdk.cmake
@@ -61,7 +61,7 @@ if(EXISTS ${_arena_sdk_conf})
 		${arena_sdk_installation_root}/lib64/libarena.so
 		${arena_sdk_installation_root}/lib64/libsave.so
 		${arena_sdk_installation_root}/lib64/libgentl.so
-		#${arena_sdk_installation_root}/lib64/liblucidlog.so
+		${arena_sdk_installation_root}/lib64/liblucidlog.so
 
 		## debug
 		#${arena_sdk_installation_root}/lib64/libarenad.so
@@ -92,7 +92,7 @@ if(EXISTS ${_arena_sdk_conf})
 		${arena_sdk_installation_root}/lib64/libarena.so
 		${arena_sdk_installation_root}/lib64/libsave.so
 		${arena_sdk_installation_root}/lib64/libgentl.so
-		#${arena_sdk_installation_root}/lib64/liblucidlog.so
+		${arena_sdk_installation_root}/lib64/liblucidlog.so
 
 		## debug
 		#${arena_sdk_installation_root}/lib64/libarenad.so
@@ -124,7 +124,7 @@ if(EXISTS ${_arena_sdk_conf})
 		${arena_sdk_installation_root}/lib/libarena.so
 		${arena_sdk_installation_root}/lib/libsave.so
 		${arena_sdk_installation_root}/lib/libgentl.so
-		#${arena_sdk_installation_root}/lib/liblucidlog.so
+		${arena_sdk_installation_root}/lib/liblucidlog.so
 
 		## debug
 		#${arena_sdk_installation_root}/lib/libarenad.so
